532105f8d603532475ad45634c69024f0622b843
Vitaly <79vitaly79@gmail.com>
2019-10-28 10:19:15 +0300

    fixed file permissions

--- file.go
+++ file.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"os"
 )
 
 func createDirectory(path string) {
+	fmt.Println(path)
 	if _, err := os.Stat(path); os.IsNotExist(err) {
-		err = os.MkdirAll(path, os.ModePerm)
+		err = os.MkdirAll(path, 0755)
 		if err != nil {
 			panic(err)
 		}
@@ -17,7 +19,7 @@ func createDirectory(path string) {
 func createFile(path string, filename string, fileText string) {
 
 	d := []byte(fileText)
-	err := ioutil.WriteFile(path+filename+".swift", d, 0644)
+	err := ioutil.WriteFile(path+filename+".swift", d, 0775)
 	if err != nil {
 		panic(err)
 	}
